1c08f531026a996f3953f66b0988ecbf57714463
remyyounes <ryounes@gmail.com>
2013-07-22 21:58:25 -0700

    test w/o ssl

--- public/scripts/screenshare.js
+++ public/scripts/screenshare.js
@@ -1,3 +1,32 @@
 $(function(){
-	
+    var hasGetUserMedia = function(){
+        return !!(navigator.webkitGetUserMedia);
+    };
+
+    var Camera = function(){
+        if (hasGetUserMedia()){
+            navigator.webkitGetUserMedia( 
+                { video: { mandatory: { chromeMediaSource: 'screen'} } },
+                this.loadMediaStream,
+                this.onUserMediaFail
+            );
+        } else {
+            alert("navigator.getUserMedia not supported in your browser!");
+        }
+    };
+
+    Camera.prototype.loadMediaStream = function(stream) {
+        console.log('!!');
+        var video = document.querySelector("video");
+        video.src = window.URL.createObjectURL(stream);
+    };
+
+    Camera.prototype.onUserMediaFail = function(){
+        console.log("denied!");
+    };
+
+    var cam  = new Camera();
+    console.log('test2');
 }());
+
+
